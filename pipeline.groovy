da30e24a61e7f844a2636d01ed8b363372054770
Christopher <minhbaovn1210@gmail.com>
2022-06-08 09:24:42 +0700

    pipeline

---
+++ pipeline.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/minhbaovn1210/my-java-spring-boot.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh './mvnw clean package'
+            }
+
+            post {
+                always {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
